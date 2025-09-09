79af28e27ea7c1e13f72709635a64e432fa7ec27
KillerDew <daniel.dew007@gmail.com>
2025-09-09 18:35:11 +0100

    Added zero lift AoA editor

--- missile-sim-unity/Assets/Scenes/Graph Coeffs/valueChangerGraphing.cs
+++ missile-sim-unity/Assets/Scenes/Graph Coeffs/valueChangerGraphing.cs
@@ -16,6 +16,9 @@ public class valueChangerGraphing : MonoBehaviour
 
     public TMP_InputField liftSlopeInputField;
 
+    public TMP_InputField zeroAoaInputField;
+    public TMP_Text zeroAoaRadText;
+
     public void onFlapFracChange(float val)
     {
         val = val / 20;
@@ -26,7 +29,7 @@ public class valueChangerGraphing : MonoBehaviour
     {
         graphingScript.flapAngle = Mathf.Deg2Rad * val;
         flapAngleIndicatorSlice.localEulerAngles = new Vector3(0, 0, -val);
-        flapAngleValueText.text = Convert.ToString(val) + ": " + Convert.ToString(Mathf.Round(graphingScript.flapAngle/Mathf.PI*100)/100) + "π rad";
+        flapAngleValueText.text = Convert.ToString(val) + "°: " + Convert.ToString(Mathf.Round(graphingScript.flapAngle/Mathf.PI*100)/100) + "π rad";
     }
     public void onLiftSlopeChange(string str)
     {
@@ -34,10 +37,18 @@ public class valueChangerGraphing : MonoBehaviour
         changingConfig.liftSlope = val;
         liftSlopeInputField.text = Convert.ToString(Mathf.Round(val/Mathf.PI*100)/100) + "π";
     }
+    public void onZeroAoaChange(string str)
+    {
+        float val = float.Parse(str);
+        changingConfig.zeroLiftAoa_deg = val;
+        zeroAoaInputField.text = Convert.ToString(Mathf.Round(val*100)/100) + "°";
+        zeroAoaRadText.text = Convert.ToString(Mathf.Round(val * Mathf.Deg2Rad / Mathf.PI * 100) / 100) + "π rad";
+    }
     void Start()
     {
         onFlapFracChange(changingConfig.flapFraction * 20);
         onFlapAngleChange(graphingScript.flapAngle * Mathf.Rad2Deg);
         onLiftSlopeChange(Convert.ToString(changingConfig.liftSlope));
+        onZeroAoaChange(Convert.ToString(changingConfig.zeroLiftAoa_deg));
     }
 }
