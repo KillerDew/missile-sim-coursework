c49a080fdc427fcaadf060d55e59140775f5b908
KillerDew <daniel.dew007@gmail.com>
2026-03-09 19:50:01 +0000

    comments on config and coeffs

--- missile-sim-unity/Assets/_Scripts/Aerodynamics/AirfoilConfig.cs
+++ missile-sim-unity/Assets/_Scripts/Aerodynamics/AirfoilConfig.cs
@@ -6,13 +6,24 @@ public class AirfoilConfig : ScriptableObject
 {
     [Header("Dimensional Properties")]
     [Range(0, 1)]
+    // Flap fraction is fraction of chord that is flap.
     public float flapFraction;
 
     [Header("Aerodynamic Behaviour")]
+    // Skin drag is the drag coefficient at 0 angle of attack.
     public float skinDrag = 0.02f;
+    /**
+    * Zero lift angle of attack (deg) is angle at which lift is zero.
+    * e.g. 0 for symmetric airfoils.
+    * Public for editing in Unity inspector. (and other scripts)
+    */
     public float zeroLiftAoa_deg;
 
     [HideInInspector]
+    /**
+    * Hidden for editing in Unity inspector, but public for other scripts to read.
+    * returns in radians, as used in calculations.
+    */
     public float zeroLiftAoa
     {
         get
@@ -20,9 +31,20 @@ public class AirfoilConfig : ScriptableObject
             return zeroLiftAoa_deg * Mathf.Deg2Rad;
         }
     }
+    // Lift per unit angle of attack in linear region. Units: rad^-1.
     public float liftSlope = 2f * Mathf.PI;
+    
+    /**
+    * Stall angle high and low (deg) are angles at which stall occurs
+    * For symmetric airfoils, stallHigh_deg = -stallLow_deg.
+    * For cambered airfoils, they can be different.
+    * Public for editing in Unity inspector. (and other scripts)
+    */
     public float stallHigh_deg;
     public float stallLow_deg;
+
+    // Stall angle high and low (rad). Hidden for editing.
+    // Read-only for other scripts.(Radians for calculations)
 
     [HideInInspector]
     public float stallAngleHigh
@@ -40,7 +62,11 @@ public class AirfoilConfig : ScriptableObject
             return stallLow_deg * Mathf.Deg2Rad;
         }
     }
+    
 
+    /**
+    * Checks if two AirfoilConfigs are equal by comparing their properties. (and reverse for !=)
+    */
     public static bool operator ==(AirfoilConfig a, AirfoilConfig b)
     {
         return a.skinDrag == b.skinDrag && a.flapFraction == b.flapFraction && a.zeroLiftAoa == b.zeroLiftAoa && a.stallAngleHigh == b.stallAngleHigh && a.stallAngleLow == b.stallAngleLow && a.liftSlope == b.liftSlope;
@@ -49,11 +75,17 @@ public class AirfoilConfig : ScriptableObject
     {
         return !(a == b);
     }
-
+    
+    /**
+    * Overrides Equals and GetHashCode so Unity can evaluate == and !=.
+    * This is important for using AirfoilConfig in collections like HashSet or Dictionary.
+    * As well as for Unity functions that rely on Equals().
+    */
     public override bool Equals(object a)
     {
         return a is AirfoilConfig && (AirfoilConfig)a == this;
     }
+    // Generates a unique hash code with all properties' hash codes.
     public override int GetHashCode()
     {
         return this.zeroLiftAoa.GetHashCode() ^ this.skinDrag.GetHashCode() ^ this.flapFraction.GetHashCode() ^ this.stallAngleHigh.GetHashCode() ^ this.stallAngleLow.GetHashCode() ^ this.liftSlope.GetHashCode();
--- missile-sim-unity/Assets/_Scripts/Aerodynamics/Coefficients.cs
+++ missile-sim-unity/Assets/_Scripts/Aerodynamics/Coefficients.cs
@@ -9,9 +9,6 @@ using UnityEngine;
 * Script based on video and project by Jump Trajectory 'Realistic Aircraft Physics for Games'
 * https://www.youtube.com/watch?v=p3jDJ9FtTyM
 */
-
-
-
 public class Coefficients
 {
 
@@ -21,24 +18,36 @@ public class Coefficients
         float friction = 1.98f - 0.0426f * flapAngle * flapAngle + 0.21f * flapAngle;
         return friction;
     }
+    /**
+    * * Calculates lift, drag and moment coefficients for angles of attack within stall limits.
+    * Based on eq.8
+    */
     static Vector3 getCoefficientsLowAoA(float alpha, AirfoilConfig config, float flapAngle, float aspectRatio)
     {
-        // cofficient calculations pre-stall based on eq. 8
+        // cofficient calculations pre-stall based on eq. 8, uses form of equation y = m(x-x0) for linear region.
         float liftCoefficient = config.liftSlope * (alpha - config.zeroLiftAoa);
+        // Induced alpha from resultant lift, based on lifting line theory.
         float inducedAlpha = liftCoefficient / (Mathf.PI * aspectRatio);
+        // Effective alpha is angle of attack adjusted for zero lift angle and induced alpha.
         float effectiveAlpha = alpha - config.zeroLiftAoa - inducedAlpha;
 
+        // Coefficients of forces normal and parallel to airfoil chord
         float tangentialCoefficient = config.skinDrag * Mathf.Cos(effectiveAlpha);
         float normalCoefficient = (liftCoefficient + tangentialCoefficient * Mathf.Sin(effectiveAlpha)) / Mathf.Cos(effectiveAlpha);
 
+        // Transformation of normal and tangential coefficients to drag using trigonometry.
         float dragCoefficient = normalCoefficient * Mathf.Sin(effectiveAlpha) + tangentialCoefficient * Mathf.Cos(effectiveAlpha);
-
+        // Moment coefficient based on eq. 8.
         float momentCoefficient = -normalCoefficient * (0.25f - 0.175f * (Mathf.Abs(effectiveAlpha) / Mathf.PI));
-
-        Vector3 coeffs = new(liftCoefficient, dragCoefficient, momentCoefficient);
+        // Return coefficients as a Vector3 for ease of use in other scripts. (x = lift, y = drag, z = moment)
+        Vector3 coeffs = new Vector3(liftCoefficient, dragCoefficient, momentCoefficient);
         return coeffs;
     }
 
+    /**
+    ** Calculates lift, drag and moment coefficients for angles of attack beyond stall limits.
+    * Based on eq.9
+    */
     static Vector3 getCoefficientsStall(float alpha, AirfoilConfig config, float flapAngle, float aspectRatio)
     {
         // Linear interpolates induced alpha at moment of stall to 0 at +- 90deg
@@ -60,22 +69,29 @@ public class Coefficients
 
         // Coefficient calculations based on eq.9, with crucial fixes found through experimentation
         // Previously, the use of sine in normal and moment coefficient calculations caused major asymetrical behaviour
-        // Found issue during python experimentation, fixed using absoloute values as suggested by Jump Trajectory
+        // This is fixed using absoloute values as suggested by Jump Trajectory
         float effectiveAlpha = alpha - config.zeroLiftAoa - inducedAlpha;
-
+        // Drag at normal is calculated using eq. 10, accounts for concave/convex shape with flap deflection
         float Cd90 = getDragAtNormal(flapAngle);
 
+        // Coefficients of forces normal and parallel to airfoil chord, based on eq. 9.
         float normalCoefficient = Cd90 * Mathf.Sin(effectiveAlpha)
             * (1f / (0.56f + 0.44f * Mathf.Abs(Mathf.Sin(effectiveAlpha))) - 0.41f * (1f - Mathf.Exp(-17 / aspectRatio)));
         float tangentialCoefficient = 0.5f * config.skinDrag * Mathf.Cos(effectiveAlpha);
 
+        // Transformation of normal and tangential coefficients to lift and drag using trigonometry.
         float liftCoefficient = normalCoefficient * Mathf.Cos(effectiveAlpha) - tangentialCoefficient * Mathf.Sin(effectiveAlpha);
         float dragCoefficient = normalCoefficient * Mathf.Sin(effectiveAlpha) + tangentialCoefficient * Mathf.Cos(effectiveAlpha);
         float momentCoefficient = -normalCoefficient * (0.25f - 0.175f * (1f - 2f * Mathf.Abs(effectiveAlpha) / Mathf.PI));
-
+        // Return coefficients as a Vector3 for ease of use in other scripts. (x = lift, y = drag, z = moment)   
         Vector3 coeffs = new(liftCoefficient, dragCoefficient, momentCoefficient);
         return coeffs;
     }
+    /**
+    * * Main function to get coefficients, public for outside use
+    * Adjusts air-foil behaviourial values depending on flap deflection and aspect ratio
+    * Uses a blending region to smooth transition at stall
+    */
     static public Vector3 getCoefficients(float alpha, AirfoilConfig config, float flapAngle, float aspectRatio)
     {
         // adjust constants
@@ -101,11 +117,12 @@ public class Coefficients
         adjustedConfig.stallHigh_deg = stallHighNew * Mathf.Rad2Deg;
         adjustedConfig.stallLow_deg = stallLowNew * Mathf.Rad2Deg;
 
-        //TODO finish corrections
+        // Within linear region.
         if (alpha >= adjustedConfig.stallAngleLow && alpha <= adjustedConfig.stallAngleHigh)
         {
             return getCoefficientsLowAoA(alpha, adjustedConfig, flapAngle, aspectRatio);
         }
+        // Outside linear region, using padded stall angles to create a blending region to smooth transition.
         else
         {
             // adjust blending region for different flap angles
@@ -119,25 +136,29 @@ public class Coefficients
 
             if (alpha > paddedStallHigh || alpha < paddedStallLow)
             {
-                // No blending is required
+                // No blending is required (outside blended region)
                 return getCoefficientsStall(alpha, adjustedConfig, flapAngle, aspectRatio);
             }
             else
             {
+                // Find coefficients at stall angle (using linear) and padded stall angle (using post-stall)
+                // Then linearly interpolate between them
                 Vector3 coeffs;
                 if (alpha > adjustedConfig.stallAngleHigh)
                 {
                     Vector3 preStall = getCoefficientsLowAoA(adjustedConfig.stallAngleHigh, adjustedConfig, flapAngle, aspectRatio);
                     Vector3 postStall = getCoefficientsStall(paddedStallHigh, adjustedConfig, flapAngle, aspectRatio);
+                    // Between 0 and 1, how far alpha is between stall angle and padded stall angle.
                     float lerpFactor = (alpha - adjustedConfig.stallAngleHigh) / (paddedStallHigh - adjustedConfig.stallAngleHigh);
-                    coeffs = Vector3.Lerp(preStall, postStall, lerpFactor);
+                    coeffs = Vector3.Lerp(preStall, postStall, lerpFactor); 
                 }
                 else
                 {
                     Vector3 preStall = getCoefficientsLowAoA(adjustedConfig.stallAngleLow, adjustedConfig, flapAngle, aspectRatio);
                     Vector3 postStall = getCoefficientsStall(paddedStallLow, adjustedConfig, flapAngle, aspectRatio);
                     float lerpFactor = (alpha - adjustedConfig.stallAngleLow) / (paddedStallLow - adjustedConfig.stallAngleLow);
-                    coeffs = Vector3.Lerp(preStall, postStall, lerpFactor);
+                    // Between 0 and 1, how far alpha is between stall angle and padded stall angle.
+                    coeffs = Vector3.Lerp(preStall, postStall, lerpFactor); // Linear interpolation
                 }
                 return coeffs;
             }
