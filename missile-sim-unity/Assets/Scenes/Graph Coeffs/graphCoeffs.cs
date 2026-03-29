18152d488002ce82365ce0d8b94e92ed640a98d8
KillerDew <daniel.dew007@gmail.com>
2026-03-29 17:34:34 +0100

    random stuff for nea

--- missile-sim-unity/Assets/Scenes/Graph Coeffs/graphCoeffs.cs
+++ missile-sim-unity/Assets/Scenes/Graph Coeffs/graphCoeffs.cs
@@ -1,84 +1,101 @@
 using System.Collections;
 using UnityEngine;
-using XCharts.Runtime;
+using XCharts.Runtime; // Charting library used to graph the coefficients
 
 public class graphCoeffs : MonoBehaviour
 {
-    public AirfoilConfig config;
-    public float flapAngle = 0f;
-    public int sampleSize = 500;
+    public AirfoilConfig config; // Airfoil being graphed
+    public float flapAngle; // Flap angle in radians, set by valueChangerGraphing script
+    public int sampleSize = 500; // No. samples to plot
 
+    // References to the charts in Unity
     public LineChart Liftchart;
     public LineChart Dragchart;
     public LineChart Momentchart;
     Serie liftSerie;
     Serie dragSerie;
     Serie momentSerie;
+    // Max and min AoAs to plot (degrees).
     public float min;
     public float max;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // Get references to the series in the charts to add data to. (Not exposed in Unity editor).
         liftSerie = Liftchart.GetSerie(0);
         dragSerie = Dragchart.GetSerie(0);
         momentSerie = Momentchart.GetSerie(0);
         StartCoroutine(graphCoeffficients());
     }
 
+    
+    float changeTimer; // Timer to delay graph update to improve visuals
+    int prevDatSize; // Previous sample size to check for changes and update graph accordingly
+    float prevMin; // Previous min AoA to check for changes and update graph accordingly
+    float prevMax; // Previous max AoA to check for changes and update graph accordingly
+    AirfoilConfig prevAirfoilConfig; // Previous airfoil config to check for changes and update graph accordingly
     // Update is called once per frame
-    float changeTimer;
-    int prevDatSize;
-    float prevMin;
-    float prevMax;
-    bool hasChanged;
-    double prevData;
-    AirfoilConfig prevAirfoilConfig;
     void Update()
     {
-        if (prevDatSize != sampleSize || prevMin != min || prevMax != max || prevAirfoilConfig != config)
+        // Check for changes in graph parameters and update graph if necessary.
+        // This allows for dynamic updates when parameters are changed.
+        bool hasChanged = prevDatSize != sampleSize || prevMin != min
+            || prevMax != max || prevAirfoilConfig != config;
+        if (hasChanged)
         {
+            // Reset timer to delay graph update for improved visuals.
             changeTimer = 1500;
-            hasChanged = false;
+            hasChanged = false; // Reset change flag until next check to prevent multiple resets.
         }
         else if (changeTimer > 0)
         {
+            // decrease timer if no changes.
             changeTimer -= Time.deltaTime * 1000;
         }
         if ((changeTimer <= 0 && !hasChanged) || Input.GetKeyDown(KeyCode.R))
         {
+            // If change timer has finished, or the R key is pressed
+            // Trigger a graph redraw using X-charts 'animation reset' procedure.
             Liftchart.AnimationReset();
             Dragchart.AnimationReset();
             Momentchart.AnimationReset();
             hasChanged = true;
         }
-
+        
+        // Update previous parameter values for next check.
         prevDatSize = sampleSize;
         prevMin = min;
         prevMax = max;
-        prevAirfoilConfig = Instantiate(config);
+        prevAirfoilConfig = Instantiate(config); // Instantiate = copy
     }
 
+    // Coroutine to graph coefficients.
+    // IEnumerator allows program to yield control when inactive.
+    const float updateTime = 0.1f; // Time between updates in seconds.
     IEnumerator graphCoeffficients()
     {
-        double x = min;
-        Vector3 y = Vector3.zero;
-        while (true)
+        double x; // AoA in degrees
+        Vector3 y; // Coefficients (x = lift, y = drag, z = moment)
+        while (gameObject.activeInHierarchy) // Run while gameobject is active in scene 
         {
-            x = min;
-            y = Vector3.zero;
-            double difference = (max-min) / sampleSize;
-            for (int i = 0; i < sampleSize; i++)
+            x = min; // Set AoA to min value
+            y = Vector3.zero; // Set coefficients to 0
+            double difference = (max-min) / sampleSize; // Calculate step size between samples
+            for (int i = 0; i < sampleSize; i++) // Loop for number of samples
             {
-                x += difference;
-                y = Coefficients.getCoefficients((float)x * Mathf.Deg2Rad, config, flapAngle, 4f);
+                x += difference; // Increment AoA by step size
+                // Get coefficients for AoA (converted to radians) and airfoil config.
+                y = Coefficients.getCoefficients((float)x * Mathf.Deg2Rad, config, 0f, 4f);
 
+                // Add data to series in charts, X: AoA: Y coefficients.
                 liftSerie.AddXYData(x, y.x);
                 dragSerie.AddXYData(x, y.y);
                 momentSerie.AddXYData(x, y.z);
 
             }
-            yield return new WaitForSeconds(0.1f);
+            yield return new WaitForSeconds(updateTime); // Yield control for specified time.
+            // Clear data before new update.
             liftSerie.ClearData();
             dragSerie.ClearData();
             momentSerie.ClearData();
--- missile-sim-unity/Assets/Scenes/Graph Coeffs/valueChangerGraphing.cs
+++ missile-sim-unity/Assets/Scenes/Graph Coeffs/valueChangerGraphing.cs
@@ -5,6 +5,30 @@ using UnityEngine.UI;
 
 public class valueChangerGraphing : MonoBehaviour
 {
+    static float resolveExp(string exp)
+    {
+        // Remove spaces
+        exp = exp.Replace(" ", "");
+        // Replace pi with its value (handles multiple cases)
+        exp = exp
+            .Replace("pi", MathF.PI.ToString())
+            .Replace("PI", MathF.PI.ToString())
+            .Replace("Pi", MathF.PI.ToString())
+            .Replace("π", MathF.PI.ToString());
+
+        // Mathematically resolve string
+        float result = 0.0f;
+        // Uses Unity's evaluator
+        bool status = ExpressionEvaluator.Evaluate(exp, out result); 
+        if (!status)
+        {
+            // Logs error and avoids crashes
+            Debug.LogError("Expression could not be evaluated."); 
+        }
+        // Returns result (or 0 if evaluation failed)
+        return result; 
+    }
+
     public AirfoilConfig changingConfig;
     public graphCoeffs graphingScript;
 
@@ -21,46 +45,47 @@ public class valueChangerGraphing : MonoBehaviour
 
     public TMP_Text skinDragValueText;
 
+    // Callback for changes to flap fraction slider.
     public void onFlapFracChange(float val)
     {
+        // Slider now goes from 0 to 20 (int values only) to allow for more precision.
+        // Conversion from 0-20 to 0-1 is done here.
         val = val / 20;
+        // Change the flap fraction in the config, and update the text to show the current value
         changingConfig.flapFraction = val;
         flapFractionValueText.text = Convert.ToString(val);
     }
+    // Callback for changes to flap angle slider.
     public void onFlapAngleChange(float val)
     {
+        // Change the flap angle in the graphing script.
         graphingScript.flapAngle = Mathf.Deg2Rad * val;
+        // Updates the the flap angle indicator and text.
         flapAngleIndicatorSlice.localEulerAngles = new Vector3(0, 0, -val);
         flapAngleValueText.text = Convert.ToString(val) + "°: " + Convert.ToString(Mathf.Round(graphingScript.flapAngle/Mathf.PI*100)/100) + "π rad";
     }
     public void onLiftSlopeChange(string str)
     {
-        float val;
-        try
-        {
-            val = float.Parse(str);
-        }
-        catch
-        {
-            val = 2 * Mathf.PI;
-        }
+        // Evaluates string and sets values
+        float val = resolveExp(str);
         changingConfig.liftSlope = val;
-        liftSlopeInputField.text = Convert.ToString(Mathf.Round(val/Mathf.PI*100)/100) + "π";
+        // Round to 2dp
+        val = Mathf.Round(val*100)/100;
+        // Displays value in field text
+        liftSlopeInputField.text = Convert.ToString(val);
     }
     public void onZeroAoaChange(string str)
     {
-        float val;
-        try
-        {
-            val = float.Parse(str);
-        }
-        catch
-        {
-            val = 0f;
-        }
+        // Evaluates string and sets values
+        float val = resolveExp(str);
         changingConfig.zeroLiftAoa_deg = val;
-        zeroAoaInputField.text = Convert.ToString(Mathf.Round(val*100)/100) + "°";
-        zeroAoaRadText.text = Convert.ToString(Mathf.Round(val * Mathf.Deg2Rad / Mathf.PI * 100) / 100) + "π rad";
+        // Displays value in field text
+        zeroAoaInputField.text = Convert.ToString(Mathf.Round(val*100)/100);
+        // Displays value in radians in seperate text display below
+        float radInPi = changingConfig.zeroLiftAoa/Mathf.PI; // In terms of pi
+        radInPi = Mathf.Round(radInPi*100)/100; // Rounds 2dp
+        // Displays pi next to it.
+        zeroAoaRadText.text = Convert.ToString(radInPi) + "π rad";
     }
     public void onSkinDragChange(float val)
     {
